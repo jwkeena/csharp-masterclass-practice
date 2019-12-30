5d93273b52207a71f28c80118b791074eb04b917
jwkeena <jwkeena@gmail.com>
2019-12-30 12:17:46 -0500

    Restructured files to correspond to lesson sections, keeping Program file clean

---
+++ CSharpPractice/Section1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Section1
+    {
+        // Variables can be declared outside or inside methods, even the Main method of Program class.
+        // But they must be declared inside the class brackets. After the namespace declarationor after the namespace brackets won't work
+        static int LanguagesLearned = 1;
+
+        // Playing with Console methods
+        public static void TurnOnChristmasMode()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Clear(); // Makes the background color apply to the whole window
+            Console.WriteLine("Christmas mode activated");
+        }
+
+        // Re assigning a variable declared at a higher scope
+        public static void ReAssignExternalVariable()
+        {
+            Console.WriteLine(LanguagesLearned);
+            // Variables defined in a higher scope can be reassigned inside a method. But variables declared inside of a method are only accessible inside that method
+            LanguagesLearned = 2;
+        }
+        
+        // First program
+        public static void HelloWorld()
+        {
+            string myName = "Justin";
+            Console.WriteLine("Hello " + myName);
+        }
+    }
+}
---
+++ CSharpPractice/Section2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Section2
+    {
+        public static void ImplicitTypeConversion()
+        {
+            int num = 7892435;
+            long bigNum = num;
+            Console.WriteLine("The integer " + num + " has been converted to a long " + bigNum + " without explicitly re casting it");
+
+            float myFloat = 13.45f; // don't forget to put an 'f' at the end of a float initialization
+            double myDouble = myFloat;
+            Console.WriteLine("The float " + myFloat + " has been converted to a double " + myDouble + " without explicitly re casting it");
+        }
+        public static void ExplicitTypeConversion()
+        {
+            double myDouble = 26.2;
+            int myInt;
+
+            // casting double to int
+            myInt = (int)myDouble; // reads as: make (=) an integer (int) out of the myDouble variable.
+            Console.WriteLine(myInt); // cuts off the decimal points to fit into int data type
+        }
+
+        // simple addition and concatenation
+        public static void DoSimpleAddition()
+        {
+            int num1 = 7;
+            int num2 = 5;
+            int sum = num1 + num2;
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+            int unassignedNum; // This value cannot be printed to the console because it has no value (unlike in JS, where its value would be null)
+        }
+    }
+}
