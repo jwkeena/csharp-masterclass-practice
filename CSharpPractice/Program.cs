dc63561b659da09fff70dabc4d6e8737fd40d78a
jwkeena <jwkeena@gmail.com>
2019-12-30 10:14:14 -0500

    Committing from .sln file

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -11,6 +11,7 @@ namespace CSharpPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Test");
             Console.ReadLine();
 
 
