b56362cc2217be6b5036985135c12fa4acc26d76
jwkeena <jwkeena@gmail.com>
2019-12-30 11:46:04 -0500

    basic test methods

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -8,12 +8,38 @@ namespace CSharpPractice
 {
     class Program
     {
+        // Variables can be declared even in the master class, outside of the Main method
+        static int LanguagesLearned = 1; 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Test");
-            Console.ReadLine();
+            // Variables defined in a higher scope can be reassigned inside a method. But variables declared inside of a method are only accessible inside that method
+            LanguagesLearned = 2; 
+            
+            // calling custom method defined later on
+            TurnOnChristmasMode();
 
+
+            // Keeps console window open
+            Console.ReadLine();
+        }
+
+        static void TurnOnChristmasMode()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.DarkGreen; 
+            Console.Clear(); // Makes the background color apply to the whole window
+            Console.WriteLine("Christmas mode activated").ToUpper();
+        }
+
+        static void SimpleIntegers()
+        {
+            // simple addition and concatenation
+            int num1 = 7;
+            int num2 = 5;
+            int sum = num1 + num2;
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+            int unassignedNum; // This value cannot be printed to the console because it has no value (unlike in JS, where its value would be null)
 
         }
     }
