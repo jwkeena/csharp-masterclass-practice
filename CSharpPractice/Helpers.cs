881799f3593eff1e67867efa3f8004a1360b2ccc
Justin Keena <45906796+jwkeena@users.noreply.github.com>
2020-01-02 10:26:09 -0500

    Adding Helpers and Section 3 classes. For real

---
+++ CSharpPractice/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Helpers
+    {
+        static public void KeepWindowOpen()
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpPractice/Section3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Section3
+    {
+        // Method creation syntax: 
+        // <Access specifier> <Return type> <Method name>(Parameter List)
+        //{
+        //  Method body
+        //}
+
+        // Practicing creating a method that returns an integer. So, instead of using 'void' for the return type, use 'int'
+        static public int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+
+    }
+}
