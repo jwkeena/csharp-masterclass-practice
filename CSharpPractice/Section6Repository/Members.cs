dab3714b55d6d6e1135521de9b72deaa36f7f927
Justin Keena <45906796+jwkeena@users.noreply.github.com>
2020-01-14 16:43:11 -0500

    2d and 3d arrays

--- CSharpPractice/Section6Repository/Members.cs
+++ CSharpPractice/Section6Repository/Members.cs
@@ -9,6 +9,8 @@ namespace CSharpPractice.Section6Repository
 {
     class Members
     {
+        // There are many kinds of members in a class. Here are most of them:
+
         // member - private field
         private string memberName;
         private string jobTitle;
@@ -54,12 +56,13 @@ namespace CSharpPractice.Section6Repository
         // member - finalizer/destructor
         // There can only be one finalizer/destructor per class (unlike constructors)
         // Can only be defined in a class, not in a struct (whatever that is)
-        // Cannot be inherited, overloaded, or called. It just fires when the object goes out of scope
+        // Cannot be inherited, overloaded, or called. It just fires automatically when the .NET framework (or core) does its garbage collecting
+        // The Garbage Collector can remove the object when it is no longer reachable, or in scope, by any other part of the program
         // So, it only exists for making cleanup statements
         ~Members() // Needs tilde, then name of the class
         {
-            Console.WriteLine("Deconstruction of Members object");
-            Debug.Write("Members object destroyed");
+            Console.WriteLine("Deconstruction of Members object"); // This one I can't even see on the console
+            Debug.Write("Members object destroyed"); // This message shows up in the Output bar
         }
     }
 }
