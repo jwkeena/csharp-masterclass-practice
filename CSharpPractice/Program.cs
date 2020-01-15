8cca08df5261a956a13b8ce7fb63e99651ced6a5
Justin Keena <45906796+jwkeena@users.noreply.github.com>
2020-01-15 12:41:37 -0500

    Tic tac toe

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -11,7 +11,7 @@ namespace CSharpPractice
             Action<object> see = Console.WriteLine;
 
             // Test section methods here
-            Section7.Create2DArray();
+            Section7TicTacToe.PlayTicTacToe();
 
 
             // Helper functions to smooth development experience
