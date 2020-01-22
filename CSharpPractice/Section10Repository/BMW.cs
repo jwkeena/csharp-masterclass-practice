a8c3e7980c0b265cba4ddbaaee5a031ffe62dbbf
Justin Keena <45906796+jwkeena@users.noreply.github.com>
2020-01-22 15:30:03 -0500

    begin section 10

---
+++ CSharpPractice/Section10Repository/BMW.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.Section10Repository
+{
+    class BMW
+    {
+    }
+}
